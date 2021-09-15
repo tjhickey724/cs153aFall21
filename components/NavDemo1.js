7fc62ad7620862dd111b2862ff05dea3395f78c1
tim <tjhickey@brandeis.edu>
2021-09-15 14:40:23 -0400

    added NavDemo1

--- components/NavDemo1.js
+++ components/NavDemo1.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { StyleSheet, Text, View, Button, } from 'react-native';
 
+import FlexDemo1Screen from './FlexDemo1'
+
+
 
 const Stack = createNativeStackNavigator();
 
@@ -11,12 +14,17 @@ const MyStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Welcome' }}
+          //options={{ title: 'Welcome' }}
         />
+
         <Stack.Screen name="Profile" component={ProfileScreen} />
+
+        <Stack.Screen name="FlexDemo1" component={FlexDemo1Screen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -25,17 +33,39 @@ const MyStack = () => {
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
-      }
-    />
+    <View style={{}}>
+
+        <Button
+          title="Go to Jane's profile"
+          onPress={() =>
+            navigation.navigate('Profile', { name: 'Jane', greeting:'Hi!' })
+               // we're passing a parameter name:'Jane' to the Profile component!
+          }
+        />
+
+        <Button
+          title="Go to Tim's profile"
+          onPress={() =>
+            navigation.navigate('Profile', { name: 'Tim', greeting:'Konichi-wa' })
+               // we're passing a parameter name:'Jane' to the Profile component!
+          }
+        />
+
+        <Button
+          title="Checkout the Flexbox demo!!"
+          onPress={() =>
+            navigation.navigate('FlexDemo1')
+          }
+        />
+    </View>
   );
 };
 
+// ProfileScreen function is called with a JSON object
+//  {navigation:..., route:...,  otherstuff}
 const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+  return <Text>{route.params.greeting}, this is {route.params.name}'s profile</Text>;
+       // we're using the parameter name passed in from the HomeScreen
 };
 
 export default MyStack;
