b81f8b161e49f22ca76f2b1d6831d6a95661eb81
tim <tjhickey@brandeis.edu>
2021-09-15 13:40:51 -0400

    tested Navigation

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Hello from './components/Hello'
 import FlexDemo1 from './components/FlexDemo1'
 import FlexDemo2 from './components/FlexDemo2'
 import FlexDemo3 from './components/FlexDemo3'
 import PRA1 from './components/PRA1'
+import NavDemo1 from './components/NavDemo1'
 
 
 // const App = () => {...}
 export default function App() {
   return (
-    <PRA1  />
+    <NavDemo1  />
   );
 }
---
+++ components/NavDemo1.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StyleSheet, Text, View, Button, } from 'react-native';
+
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+export default MyStack;
