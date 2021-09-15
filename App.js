7d765ecce1a82ea470982e8fb5c099ce31b49ea4
tim <tjhickey@brandeis.edu>
2021-09-15 14:52:37 -0400

    cleaned up App.js

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-/*
-import FlexDemo1 from './components/FlexDemo1'
-import FlexDemo2 from './components/FlexDemo2'
-import FlexDemo3 from './components/FlexDemo3'
-import PRA1 from './components/PRA1'
-*/
+
 import NavDemo1 from './components/NavDemo1'
 
 
