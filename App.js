2515ee7f7d5da35540a9397ec1e5602bc37be5dd
tim <tjhickey@brandeis.edu>
2021-10-06 13:10:22 -0400

    cleaned up code

--- App.js
+++ App.js
@@ -1,37 +1,8 @@
 import React from 'react';
-/*
-import {View} from 'react-native';
-import NavDemo1 from './components/NavDemo1'
-import Quiz1 from './components/Quiz1.js'
-import Quiz1a from './components/Quiz1a.js'
-import Grading from './components/Grading.js'
-import PropDemo from './components/PropDemo.js'
-import TipCalculator from './components/TipCalculator.js'
-import Counter from './components/Counter.js'
-import ListDemoScreen from './components/ListDemoScreen'
-//import CovidDemo from './components/CovidDemo'
-import PhotoId from './components/PhotoId'
-import ShowPage from './components/ShowPage'
-
-import ShowPage from './components/ShowPage'
-import Counter from './components/Counter.js'
-import WebViewer from './components/WebViewer'
-
-import Profile from './components/Profile'
-import Pomodoros from './components/Pomodoros'
-import WebViewer from './components/WebViewer'
-import CounterDemo from './components/CounterDemo'
-import ContextDemo from './components/ContextDemo'
-import NavDemo from './components/NavContextDemo'
-*/
-
-
-//import NavDemo from './components/NavContextDemo'
 import MathQuiz from './components/MathQuiz'
 
 export default function App() {
   return (
-    <MathQuiz n={12}/>
-
+    <MathQuiz n={100}/>
   );
 }
